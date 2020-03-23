a8c69e0b85f87a961205b5d64bafd45aac798d1d
jmclean-coder <joshmclena@gmail.com>
2020-03-22 22:59:44 -0700

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,31 @@
 class Waiter
+    attr_reader :name, :yrs_experience
 
+    @@all = []
+
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+    # initializes a meal using the current Waiter instance, a provided Customer instance and a total and tip
+  end
+
+  def meals
+    Meal.all.select {|meal| meal.waiter == self}
+  end
+
+  def best_tipper
+    big_tip = meals.max { |meal_a, meal_b| 
+        meal_a.tip <=> meal_b.tip}
+  big_tip.customer
+end
+# binding.pry
 end
